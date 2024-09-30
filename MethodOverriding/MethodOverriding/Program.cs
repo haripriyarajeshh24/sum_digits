843da33de86f09e69744eedef1f3a5e858cd9000
haripriya <haripriyamkrajesh@gmail.com>
2024-09-30 16:42:46 +0530

    methodoverloading

---
+++ MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+namespace MethodOverloadingCsharp
+{
+    class Logger
+    {
+        public void log(string classname, string methodname, string errormessage)
+        {
+            Console.WriteLine(classname + ", " + methodname + ", " + errormessage);
+        }
+        public void log(string errormessage)
+        {
+            Console.WriteLine(errormessage);
+        }
+        public void log(string classname, string errormessage)
+        {
+            Console.WriteLine(classname + ", " + errormessage);
+        }
+        public void log(string classname, string methodname, string dateTime, string errormessage)
+        {
+            DateTime result = DateTime.Parse(dateTime);
+            Console.WriteLine(classname + ", " + methodname + ", " + dateTime + ", " + errormessage);
+        }
+        static void Main(string[] args)
+        {
+            Logger logobj = new Logger();
+            logobj.log("logger", "method a", "No error");
+            logobj.log("No error");
+            logobj.log("logger", "No error");
+            logobj.log("logger", "method a", "16/09/2019", "No error");
+            Console.ReadLine();
+        }
+    }
+}
