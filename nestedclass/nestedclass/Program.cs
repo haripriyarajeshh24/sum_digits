6987cdb463bca5f39fc9698d0169399d1fc11d3f
haripriya <haripriyamkrajesh@gmail.com>
2024-09-30 13:41:17 +0530

    nestedclasses

---
+++ nestedclass/nestedclass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace NestedClass
+{
+
+    // outer class
+    public class Cpu
+    {
+
+        // inner class
+        public class Ram
+        {
+            public string memory = "memory";
+            public string manufacturer = "manufacturer";
+            public void getClockspeed()
+            {
+                Console.WriteLine("Got Clock speed");
+            }
+        }
+        public class Processor
+        {
+            public string manufacturer = "manufacturer";
+            public string cores = "core";
+            public void getCache()
+            {
+                Console.WriteLine("Got Cache");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cpu.Processor processor = new Cpu.Processor();
+            Cpu.Ram ram = new Cpu.Ram();
+            ram.getClockspeed();
+            processor.getCache();
+            Console.ReadLine();
+
+        }
+    }
+}
